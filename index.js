55cd7d8922f146dfa0b22af11f209c0f0daa5f73
aungureanu614 <aungureanu614@gmail.com>
2016-08-25 13:50:51 -0700

    index.js

--- index.js
+++ index.js
@@ -1,42 +1,28 @@
 var state = {
-    items: []
+    items: [],
+    answers: [
+        '0815',
+        '2B',
+        'BAM128',
+        'Barely'
+    ],
 };
 
 var questions = [{
     text: '<:48:x<:65:=<:6C:$=$=$$~<:03:+$~<:ffffffffffffffbd:+$<:ffffffffffffffb1:+$<:57:~$~<:18:x+$~<:03:+$~<:06:x-$x<:0e:x-$=x<:43:x-$',
-    answers: [
-        '0815',
-        '2B',
-        'BAM128',
-        'Barely'
-    ],
+
     correct: 0
 }, {
     text: '+0+0+0+0+0+0+0+2)+0+0+9)+7))+3)-0-0-0-0-0-0-0-9)+0+0+0+0+0+0+0+0+7)-8)+3)-6)-8)-7-0-0-0-0-0-0)',
-    answers: [
-        '0815',
-        '2B',
-        'BAM128',
-        'Barely'
-    ],
+
     correct: 1
 }, {
     text: '*6*3p*4*3*2*0p*2*1*0pp>0*1*0p*5*4*0p*5*4*2*1*0p*4*3p*1*0p/+0p+0*6*5*2p+0*5*0p',
-    answers: [
-        '0815',
-        '2B',
-        'BAM128',
-        'Barely'
-    ],
+
     correct: 2
 }, {
     text: ']xhhhhooooooooohhhhhhxooooooooxooooooxjjjxhoooohhhxhohhhhhhhxhhhhjjjhhhxhhhhooooooooohhhhhhxjjjxxjjjjjjjxjhhhhxjhhhhhhhhjjjhh~',
-    answers: [
-        '0815',
-        '2B',
-        'BAM128',
-        'Barely'
-    ],
+
     correct: 3
 }];
 
@@ -44,8 +30,10 @@ var questions = [{
 
 var addItem = function(state, question) {
     state.items.push(question);
-
+    
 };
+
+//console.log(state.items[0]);
 
 
 var renderQuestion = function(state, element) {
@@ -58,10 +46,11 @@ var renderQuestion = function(state, element) {
 
 };
 
-var renderAnswers = function(state, element) {
-    var answersHTML = state.items.map(function(question) {
 
-        return '<li>' + question.answers + '</li>';
+var renderAnswers = function(state, element) {
+    var answersHTML = state.answers.map(function(answer) {
+
+        return '<li>' + answer + '</li>';
     });
 
     element.html(answersHTML);
